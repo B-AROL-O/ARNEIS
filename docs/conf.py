87c64b398be90c0d31932b009dc0342a8d6d7b06
Gianpaolo Macario <gmacario@gmail.com>
2022-01-19 22:59:37 +0100

    Fix flake8 rule E124
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ extensions = ['myst_parser']
 # Add the Markdown filename extension to the `source_suffix` list.
 # Reference: https://www.sphinx-doc.org/en/master/usage/markdown.html
 source_suffix = {'.rst': 'restructuredtext', '.txt': 'markdown', '.md': 'markdown'
-}
+                }
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
